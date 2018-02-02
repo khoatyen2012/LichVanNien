a8e218c2d307ba8e111901d619d71c737decf92c
khoatyen2012 <33951988+khoatyen2012@users.noreply.github.com>
2018-02-02 17:00:26 +0700

    giao dien altp
    bat dau

--- app/src/main/java/tana/daithanh/lichvannien/AiLaTrieuPhu.java
+++ app/src/main/java/tana/daithanh/lichvannien/AiLaTrieuPhu.java
@@ -2,12 +2,23 @@ package tana.daithanh.lichvannien;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ViewFlipper;
 
 public class AiLaTrieuPhu extends Activity {
+
+    ViewFlipper viewFlipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ai_la_trieu_phu);
+
+        viewFlipper =(ViewFlipper) findViewById(R.id.vfALTP);
+    }
+
+    public  void  onClickPlay(View view)
+    {
+        viewFlipper.setDisplayedChild(1);
     }
 }
