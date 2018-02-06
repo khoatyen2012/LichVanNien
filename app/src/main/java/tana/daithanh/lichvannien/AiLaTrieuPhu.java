cebd79b93b53d23cf9da6aaabf22c72a7785614a
khoatyen2012 <33951988+khoatyen2012@users.noreply.github.com>
2018-02-06 16:46:13 +0700

    onClick BtAD
    a

--- app/src/main/java/tana/daithanh/lichvannien/AiLaTrieuPhu.java
+++ app/src/main/java/tana/daithanh/lichvannien/AiLaTrieuPhu.java
@@ -7,9 +7,12 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.ViewFlipper;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import tana.daithanh.database.DataSourceALTP;
 import tana.daithanh.database.Question;
@@ -24,6 +27,15 @@ public class AiLaTrieuPhu extends Activity {
     boolean alowMP3 = false;
     Handler handler = new Handler();
     private int level = 1;
+    TextView tvQuestion;
+    TextView btDaa;
+    TextView btDab;
+    TextView btDac;
+    TextView btDad;
+    private int mCaseTrue = 0;
+    private int mSelect = 0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +43,11 @@ public class AiLaTrieuPhu extends Activity {
         setContentView(R.layout.activity_ai_la_trieu_phu);
 
         viewFlipper =(ViewFlipper) findViewById(R.id.vfALTP);
+        tvQuestion=(TextView) findViewById(R.id.tvQuestion);
+        btDaa=(TextView)findViewById(R.id.btA);
+        btDab=(TextView)findViewById(R.id.btB);
+        btDac=(TextView)findViewById(R.id.btC);
+        btDad=(TextView)findViewById(R.id.btD);
 
         datasource = new DataSourceALTP(this);
         datasource.open();
@@ -41,6 +58,134 @@ public class AiLaTrieuPhu extends Activity {
 
     }
 
+
+    public  void  onClickBtA(View view)
+    {
+        try {
+            if (mSelect == 0) {
+                mSelect=1;
+                doCallSound("da.mp3");
+                btDaa.setBackgroundResource(R.drawable.answer_panel_select);
+            }
+        } catch (Exception ex) {
+
+        }
+    }
+
+    public  void  onClickBtB(View view)
+    {
+        try {
+            if (mSelect == 0) {
+                mSelect=2;
+                doCallSound("db.mp3");
+                btDab.setBackgroundResource(R.drawable.answer_panel_select);
+            }
+        } catch (Exception ex) {
+
+        }
+    }
+
+    public  void  onClickBtC(View view)
+    {
+        try {
+            if (mSelect == 0) {
+                mSelect=3;
+                doCallSound("dc.mp3");
+                btDac.setBackgroundResource(R.drawable.answer_panel_select);
+            }
+        } catch (Exception ex) {
+
+        }
+    }
+
+    public  void  onClickBtD(View view)
+    {
+        try {
+            if (mSelect == 0) {
+                mSelect=4;
+                doCallSound("dd.mp3");
+                btDad.setBackgroundResource(R.drawable.answer_panel_select);
+            }
+        } catch (Exception ex) {
+
+        }
+    }
+
+
+
+         /*
+    Tu dong chon dap an theo level
+     */
+
+    void subget() {
+
+        try {
+            //showDemNguoc();
+
+           // ivLaiVanSam.setImageResource(R.drawable.hoi);
+            if (level == 1) {
+
+                doCallSound("hoi1.mp3");
+               // handler.removeCallbacks(myStart);
+            } else if (level == 2) {
+                doCallSound("hoi2.mp3");
+            } else if (level == 3) {
+                doCallSound("hoi3.mp3");
+            } else if (level == 4) {
+                doCallSound("hoi4.mp3");
+            } else if (level == 5) {
+                doCallSound("hoi5.mp3");
+
+
+            } else if (level == 6) {
+                doCallSound("hoi6.mp3");
+            } else if (level == 7) {
+                doCallSound("hoi7.mp3");
+            } else if (level == 8) {
+                doCallSound("hoi8.mp3");
+            } else if (level == 9) {
+                doCallSound("hoi9.mp3");
+            } else if (level == 10) {
+                doCallSound("hoi10.mp3");
+
+            } else if (level == 11) {
+                doCallSound("hoi11.mp3");
+            } else if (level == 12) {
+                doCallSound("hoi12.mp3");
+            } else if (level == 13) {
+                doCallSound("hoi13.mp3");
+            } else if (level == 14) {
+                doCallSound("hoi14.mp3");
+            } else if (level == 15) {
+                doCallSound("hoi15.mp3");
+
+            }
+
+            ArrayList<Question> lstTMG = new ArrayList<Question>();
+            for (int i = 0; i < lstVN.size(); i++) {
+                if (Integer.parseInt(lstVN.get(i).getLevel()) != level) {
+                    continue;
+                }
+                lstTMG.add(lstVN.get(i));
+            }
+
+            Random rd = new Random();
+            int k = rd.nextInt(lstTMG.size());
+            tvQuestion.setText("Câu " + level + ": " + lstTMG.get(k).getQuestion());
+            btDaa.setText("A: " + lstTMG.get(k).getCasea());
+            btDab.setText("B: " + lstTMG.get(k).getCaseb());
+            btDac.setText("C: " + lstTMG.get(k).getCasec());
+            btDad.setText("D: " + lstTMG.get(k).getCased());
+            mCaseTrue = Integer.parseInt(lstTMG.get(k).getTruecase());
+//            mNamMuoi = 0;
+//            doRank(level);
+        } catch (Exception ex) {
+
+        }
+
+    }
+
+
     /*
 run loai bo
 */
@@ -49,6 +194,7 @@ run loai bo
         public void run() {
             handler.removeCallbacks(myLoadSha);
             onClickShowBackRight(1);
+            subget();
         }
     };
 
